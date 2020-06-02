5f39fa3255f4c9d3ae673a2afcd77ce1baa6672c
raviteja155 <raviteja155@gmail.com>
2020-06-02 20:40:19 +0530

    Initial render.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+function PostsList(props) {
+  const posts = props.data;
+
+  const list = posts.map(post => 
+    <div className="post">
+      <h3 className="mb-0">{post.title}</h3>
+      <p className="mt-0">{post.likes} Likes</p>
+    </div>
+  );
+
+  return (
+    <div>
+      {list}
+    </div>
+  )
+}
+
+function Feed() {
+  const data = [{
+    "id": 1,
+    "title": "My first post",
+    "likes": 0,
+    "comments": [{
+      "body": "This is the first comment.",
+      "addedBy": "Ravi teja"
+    }]
+  }, {
+    "id": 2,
+    "title": "My second post title",
+    "likes": 12,
+    "comments": []
+    }];
+
+  return (
+    <PostsList data={data}/>
+  )
+}
+
+function Title() {
+  return (
+    <div>
+      <h2>Components With React</h2>
+      <p>The base component <strong>App</strong> renders child components below.</p>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <small>Thank You!</small>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <Feed />
+      <Footer />
     </div>
   );
 }
